1f4735bcc4f112c3e43d5d46775b0cc99f1a6b1f
Niall Scarlett <nscarlett@nlcsjeju.kr>
2026-09-20 11:50:38 +0900

    firebase-config.js

--- python-functions-lesson/firebase-config.js
+++ python-functions-lesson/firebase-config.js
@@ -1,11 +1,11 @@
 // Paste your Firebase web app settings here (see README.md, step 4).
 // These values are safe to publish: access is controlled by the database rules.
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE.firebaseapp.com",
-  databaseURL: "https://PASTE-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "PASTE",
-  storageBucket: "PASTE.appspot.com",
-  messagingSenderId: "PASTE",
-  appId: "PASTE"
+  apiKey: "AIzaSyAQ4xkIKTO-hFPag-vyxi1m5Cgs5XDl_Pw",
+    authDomain: "cs-class-votes.firebaseapp.com",
+    databaseURL: "https://cs-class-votes-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "cs-class-votes",
+    storageBucket: "cs-class-votes.firebasestorage.app",
+    messagingSenderId: "735909226419",
+    appId: "1:735909226419:web:156d63e33985f75647cdc6"
 };
